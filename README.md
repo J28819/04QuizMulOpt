a36365ca7e143dcb9778335ecb48984cd501c12f
BC_IML <lelj28819@gmail.com>
2022-05-17 20:28:47 -0400

    First commit adding folder structure and first objects for Question and Answers for the quiz

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # 04QuizMulOpt
 Quiz with Multiple options and Score Results
+
+
+
+## Locations 
+
+Index.html -> Contain all the code that will be processes by the browser to display in the end user display assets -> contain all the formating code (CSS) and pictures that will be displayed in the front end.
+
+## Installation
+
+1. To clone the repository in your computer run this command.
+
+```bash
+git clone git@github.com:J28819/04QuizMulOpt.git
+
+```
+
+2. Then open it with Visual Code and run Live Server on the [Index.html file.]
+
+3. You can Follow this link : 
+
+
+
+## Description
+
+This Quiz has multiple options, and use a JSON Object to customize the questions its also use local storage to record and shows the last Highest Scores.
+
+## Screenshoots
+
+![My animated logo](assets/readme-pics/overview.png)
+
+## Conclusion 
+
+It was a great experience works in this project.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ assets/js/quiz.js
@@ -0,0 +1,43 @@
+
+var Questions = [
+                {
+                "Q": "Commonly used data types DO NOT include:",
+                "1": "Strings",
+                "2": "Booleans",
+                "3": "Alerts",
+                "4": "Numbers",
+                "R": "3"        
+                },
+                   {
+                "Q": "The condition in an if/else statment is enclosed within ______ .",
+                "1": "Quotes",
+                "2": "Curly Brackets",
+                "3": "Parentheses",
+                "4": "Square Brackets",
+                "R": "2"        
+                },
+                   {
+                "Q": "Arrays in Javascript can be used to store ______ .",
+                "1": "Numbers and Strings",
+                "2": "Other Arrays",
+                "3": "Booleans",
+                "4": "All of the above",
+                "R": "4"        
+                },
+                   {
+                "Q": "String values must be enclosed within ______ when being assigned to variables.",
+                "1": "Commas",
+                "2": "Curly brackets",
+                "3": "Quotes",
+                "4": "Parenthesis",
+                "R": "4"        
+                },
+                 {
+                "Q": "A very useful tool used during development and debugging for printing content to the debugger is:",
+                "1": "Javascript",
+                "2": "Terminal / Bash",
+                "3": "For loops",
+                "4": "Console.log()",
+                "R": "4"        
+                }
+            ]
